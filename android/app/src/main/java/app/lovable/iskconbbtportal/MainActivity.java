63881a13d73234bb3853864462ea75d9c42ff5ff
abhy12 <98876719+abhy12@users.noreply.github.com>
2025-07-06 14:22:57 +0530

    remove

--- android/app/src/main/java/app/lovable/iskconbbtportal/MainActivity.java
+++
@@ -1,6 +0,0 @@
-
-package app.lovable.iskconbbtportal;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- supabase/migrations/20250609190518-c9aae258-f0dd-4c1e-894c-a63c64467645.sql
+++
@@ -1,4 +0,0 @@
-
--- Enable real-time for meeting_attendees table
-ALTER TABLE public.meeting_attendees REPLICA IDENTITY FULL;
-ALTER PUBLICATION supabase_realtime ADD TABLE public.meeting_attendees;
